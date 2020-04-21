5d2e7b3d29b9eb5bca0ebb6f155f610f3fdbe7a4
Anthony Gomes <code@tonygom.es>
2020-04-20 18:58:53 -0600

    Add an upate spec for Discounts

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -17,6 +17,20 @@ class Merchant::DiscountsController < Merchant::BaseController
     end
   end
 
+  def edit
+    @discount = Discount.find(params[:id])
+  end
+
+  def update
+    @discount = Discount.find(params[:id])
+    if @discount.update(discount_params)
+      redirect_to "/merchant/discounts"
+    else
+      generate_flash(@discount)
+      render :edit
+    end
+  end
+
   def destroy
     discount = Discount.find(params[:id])
     discount.destroy
